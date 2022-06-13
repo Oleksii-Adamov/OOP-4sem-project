608031722ef85b2c060f1c2157259edf42e4d00a
PavelKilko <pavel.kilko@gmail.com>
2022-06-13 11:13:26 +0300

    Init server

---
+++ Server/main.cpp
@@ -0,0 +1,71 @@
+#include "../common_src/net.h"
+
+enum class CustomMsgTypes : uint32_t
+{
+	ServerAccept,
+	ServerPing,
+	MessageAll,
+	ServerMessage,
+};
+
+class CustomServer : public net::server_interface<CustomMsgTypes>
+{
+public:
+	CustomServer(uint16_t nPort) : net::server_interface<CustomMsgTypes>(nPort)
+	{
+	
+	}
+
+protected:
+	virtual bool OnClientConnect(std::shared_ptr<net::connection<CustomMsgTypes>> client)
+	{
+		net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::ServerAccept;
+		client->Send(msg);
+		return true;
+	}
+	
+	virtual void OnClientDisconnect(std::shared_ptr<net::connection<CustomMsgTypes>> client)
+	{
+		std::cout << "Removing client [" << client->GetID() << "]\n";
+	}
+	
+	virtual void OnMessage(std::shared_ptr<net::connection<CustomMsgTypes>> client, net::message<CustomMsgTypes>& msg)
+	{
+		switch (msg.header.id)
+		{
+			case CustomMsgTypes::ServerPing:
+			{
+				std::cout << "[" << client->GetID() << "]: Server Ping\n";
+				
+				client->Send(msg);
+			}
+				break;
+			
+			case CustomMsgTypes::MessageAll:
+			{
+				std::cout << "[" << client->GetID() << "]: Message All\n";
+				
+				net::message<CustomMsgTypes> msg;
+				msg.header.id = CustomMsgTypes::ServerMessage;
+				msg << client->GetID();
+				MessageAllClients(msg, client);
+				
+			}
+				break;
+		}
+	}
+};
+
+int main()
+{
+	CustomServer server(60000);
+	server.Start();
+	
+	while (1)
+	{
+		server.Update(-1, true);
+	}
+	
+	return 0;
+}
