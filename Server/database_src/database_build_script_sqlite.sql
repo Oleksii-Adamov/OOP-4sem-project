15eda85f8822245461fe693671df7464851b59e3
Artem Poliakov <artem4250@gmail.com>
2022-06-13 18:48:39 +0300

    database_src fix

--- Server/database_src/database_build_script_sqlite.sql
+++ Server/database_src/database_build_script_sqlite.sql
@@ -3,45 +3,45 @@
 -- Table `User`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `User` (
-  `UserID` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `Login` VARCHAR(20) NOT NULL,
+  `UserID` INTEGER UNIQUE NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `Login` VARCHAR(20) UNIQUE NOT NULL,
   `UserName` VARCHAR(20) NOT NULL);
-CREATE UNIQUE INDEX `UserID_UNIQUE` ON `User`(`UserID`);
-CREATE UNIQUE INDEX `Login_UNIQUE` ON `User`(`Login`);
+CREATE UNIQUE INDEX IF NOT EXISTS `UserID_UNIQUE` ON `User`(`UserID`);
+CREATE UNIQUE INDEX IF NOT EXISTS `Login_UNIQUE` ON `User`(`Login`);
 
 
 -- -----------------------------------------------------
 -- Table `Authorization`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Authorization` (
-  `UserID` INTEGER NOT NULL PRIMARY KEY,
+  `UserID` INTEGER UNIQUE NOT NULL PRIMARY KEY,
   `Password` VARCHAR(66) NOT NULL,
   FOREIGN KEY (`UserID`) REFERENCES `User`(`UserID`) ON UPDATE CASCADE ON DELETE CASCADE);
-CREATE UNIQUE INDEX `UserID1_UNIQUE` ON `Authorization`(`UserID`);
+CREATE UNIQUE INDEX IF NOT EXISTS `UserID1_UNIQUE` ON `Authorization`(`UserID`);
 
 
 -- -----------------------------------------------------
 -- Table `Class`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Class` (
-  `ClassID` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `ClassID` INTEGER UNIQUE NOT NULL PRIMARY KEY AUTOINCREMENT,
   `TeacherUserID` INT NOT NULL,
   FOREIGN KEY (`TeacherUserID`) REFERENCES `User`(`UserID`) ON UPDATE CASCADE ON DELETE CASCADE);
-CREATE UNIQUE INDEX `RoomID_UNIQUE` ON `Class`(`ClassID`);
-CREATE INDEX `fk_Class_User_idx` ON `Class`(`TeacherUserID`);
+CREATE UNIQUE INDEX IF NOT EXISTS `RoomID_UNIQUE` ON `Class`(`ClassID`);
+CREATE INDEX IF NOT EXISTS `fk_Class_User_idx` ON `Class`(`TeacherUserID`);
 
 
 -- -----------------------------------------------------
 -- Table `Assignment`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Assignment` (
-  `AssignmentID` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `AssignmentID` INTEGER UNIQUE NOT NULL PRIMARY KEY AUTOINCREMENT,
   `TeacherUserID` INTEGER NOT NULL,
-  `AssignmenCreationDate` DATETIME NOT NULL,
+  `AssignmentCreationDate` DATETIME NOT NULL DEFAULT current_timestamp,
   `AssignmentData` VARCHAR(66) NOT NULL,
   FOREIGN KEY (`TeacherUserID`) REFERENCES `User`(`UserID`) ON UPDATE CASCADE ON DELETE CASCADE);
-CREATE UNIQUE INDEX `TaskID_UNIQUE` ON `Assignment`(`AssignmentID`);
-CREATE INDEX `fk_Assignment_User1_idx` ON `Assignment`(`TeacherUserID`);
+CREATE UNIQUE INDEX IF NOT EXISTS `TaskID_UNIQUE` ON `Assignment`(`AssignmentID`);
+CREATE INDEX IF NOT EXISTS `fk_Assignment_User1_idx` ON `Assignment`(`TeacherUserID`);
 
 
 -- -----------------------------------------------------
@@ -52,8 +52,8 @@ CREATE TABLE IF NOT EXISTS `Student_Class` (
   `ClassID` INTEGER NOT NULL,
   FOREIGN KEY (`StudentUserID`) REFERENCES `User`(`UserID`) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY (`ClassID`) REFERENCES `Class`(`ClassID`) ON UPDATE CASCADE ON DELETE CASCADE);
-CREATE INDEX `fk_User_has_Class_User1_idx` ON `Student_Class`(`StudentUserID`);
-CREATE INDEX `fk_User_has_Class_Class1_idx` ON `Student_Class`(`ClassID`);
+CREATE INDEX IF NOT EXISTS `fk_User_has_Class_User1_idx` ON `Student_Class`(`StudentUserID`);
+CREATE INDEX IF NOT EXISTS `fk_User_has_Class_Class1_idx` ON `Student_Class`(`ClassID`);
 
 
 -- -----------------------------------------------------
@@ -65,5 +65,5 @@ CREATE TABLE IF NOT EXISTS `Student_Assignment` (
   `StudentAssignmentAnswer` VARCHAR(66) NULL,  
   FOREIGN KEY (`AssignmentID`) REFERENCES `Assignment`(`AssignmentID`) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY (`StudentUserID`) REFERENCES `User`(`UserID`) ON UPDATE CASCADE ON DELETE CASCADE);
-CREATE INDEX `fk_Assignment_has_User_User1_idx` ON `Student_Assignment`(`StudentUserID`);
-CREATE INDEX `fk_Assignment_has_User_Assignment1_idx` ON `Student_Assignment`(`AssignmentID`);
+CREATE INDEX IF NOT EXISTS `fk_Assignment_has_User_User1_idx` ON `Student_Assignment`(`StudentUserID`);
+CREATE INDEX IF NOT EXISTS `fk_Assignment_has_User_Assignment1_idx` ON `Student_Assignment`(`AssignmentID`);
