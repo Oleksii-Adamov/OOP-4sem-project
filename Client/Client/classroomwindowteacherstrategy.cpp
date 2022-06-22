364248d342f0b006f7fdf00db3ca70b6652765ac
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2022-06-22 09:51:29 +0300

    added more handling of messages

--- Client/Client/classroomwindowteacherstrategy.cpp
+++ Client/Client/classroomwindowteacherstrategy.cpp
@@ -26,30 +26,29 @@ void ClassroomWindowTeacherStrategy::SetModel(QSharedPointer<QAbstractListModel>
 
 void ClassroomWindowTeacherStrategy::Update(net::message<CustomMsgTypes>& msg, QSharedPointer<QAbstractListModel>& model)
 {
-    if (msg.header.id == CustomMsgTypes::RETURN_ALL_ASSIGNMENTS_OF_CLASSROOM_STUDENT_INFO) {
-   /* StudentAssignmentSessionsListModel* student_assignment_sessions_model = qobject_cast<StudentAssignmentSessionsListModel*>(model.data());
-    //student_assignment_sessions_model->PushBack(StudentAssignmentSessionInfo(StudentAssignmentSession(1,1,StudentAssignmentSessionStatus::not_submitted, "", 0, ""),
-     student_assignment_sessions_model->Clear();
-     QJsonDocument json_doc = QJsonDocumentFromServerMessage(msg);
-     QJsonObject json_doc_obj = json_doc.object();
-     QJsonArray infos =  json_doc_obj.take("StudentAssignmentSessionInfos").toArray();
-     for (int i = 0; i < infos.size(); i++)
-     {
-         QJsonObject info_object = infos.at(i).toObject();
-         QJsonObject student_assignment_session_object = info_object.take("StudentAssignmentSession").toObject();
-         QJsonObject assignment_session_obj = info_object.take("AssignmentSession").toObject();
-         QJsonObject assignment_obj = info_object.take("Assignment").toObject();
-         Assignment assignment = GetAssignmentFromJson(assignment_obj);
-         AssignmentSession AssignmentSession = GetAssignmentSessionFromJson(assignment_session_obj);
-         StudentAssignmentSession student_assignment_session = GetStudentAssignmentSessionFromJson(student_assignment_session_object);
-     }*/
+    if (msg.header.id == CustomMsgTypes::RETURN_ALL_ASSIGNMENTS_OF_CLASSROOM_TEACHER_INFO) {
+        AssignmentSessionsListModel* student_assignment_sessions_model = qobject_cast<AssignmentSessionsListModel*>(model.data());
+        student_assignment_sessions_model->Clear();
+        QJsonDocument json_doc = QJsonDocumentFromServerMessage(msg);
+        QJsonObject json_doc_obj = json_doc.object();
+        QJsonArray infos =  json_doc_obj.take("StudentAssignmentSessionInfos").toArray();
+        for (int i = 0; i < infos.size(); i++)
+        {
+            QJsonObject info_object = infos.at(i).toObject();
+            QJsonObject student_assignment_session_object = info_object.take("StudentAssignmentSession").toObject();
+            QJsonObject assignment_session_obj = info_object.take("AssignmentSession").toObject();
+            QJsonObject assignment_obj = info_object.take("Assignment").toObject();
+            Assignment assignment = GetAssignmentFromJson(assignment_obj);
+            AssignmentSession assignment_session = GetAssignmentSessionFromJson(assignment_session_obj);
+           student_assignment_sessions_model->PushBack(AssignmentSessionInfo(assignment_session, assignment));
+        }
     }
 }
 
 void ClassroomWindowTeacherStrategy::GetData(const ClassroomInfo& classroom_info)
 {
     net::message<CustomMsgTypes> message;
-    message.header.id = CustomMsgTypes::GET_ALL_ASSIGNMENTS_OF_CLASSROOM_STUDENT_INFO;
+    message.header.id = CustomMsgTypes::GET_ALL_ASSIGNMENTS_OF_CLASSROOM_TEACHER_INFO;
     message << classroom_info.classroom.getClassroomId() << Client::GetInstance()->GetUser().getUserId();
     Client::GetInstance()->Send(message);
 }
